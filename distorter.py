23f426871a37c588e135a5ed35985da5ae93fe7f
brandon-gable <brandonryangable@gmail.com>
2025-03-05 22:51:09 -0500

    initial commit

---
+++ distorter.py
@@ -0,0 +1,19 @@
+import os
+from pydub import AudioSegment
+
+# Reduces bitrate of the audio file
+def makeAudioSoundBad(audio: AudioSegment, filePath: str) -> AudioSegment:
+    filePath = filePath.replace("\\", "/")
+    fileDir = os.path.dirname(filePath)
+    newFileName = input("Enter the resulting file name:\n")
+    if not newFileName.endswith(".mp3"):
+        newFileName += ".mp3"
+    newAudioPath = os.path.join(fileDir, newFileName)
+    newAudioFile = audio.export(newAudioPath, format="mp3", bitrate="4k")
+    print("file saved as " + fileDir + newFileName)
+    return newAudioFile
+
+filePath = input("Enter the file path (and name)\n(must be an mp3): ")
+audio = AudioSegment.from_mp3(filePath)
+badAudio = makeAudioSoundBad(audio, filePath)
+print("The worsened file is in the same directory as the original!")
